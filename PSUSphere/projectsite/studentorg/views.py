2344ae1a348124b8b07e41977a37c91925c9319c
janivn <202080160@psu.palawan.edu.ph>
2024-04-19 14:19:12 +0800

    Done

--- PSUSphere/projectsite/studentorg/views.py
+++ PSUSphere/projectsite/studentorg/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    content_objective_name = 'home'
+    template_name = "home.html"
